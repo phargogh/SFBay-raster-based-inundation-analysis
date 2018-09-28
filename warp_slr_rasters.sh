e602a37fa0cb36cc1dfc95cbcd3fb424ea7993a7
James Douglass <jdouglass@stanford.edu>
2018-09-28 12:29:22 -0700

    Initializing warped pixel values to 0.

--- warp_slr_rasters.sh
+++ warp_slr_rasters.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash -ex
 
 ######################################################################
 # @author      : jdouglass (jdouglass@DN0a230095.SUNet)
@@ -11,7 +11,7 @@
 mask_rasters_dir=data/jd_masked_slr
 reprojected_dir=data/jd_slr/
 
-mkdir -pf $reprojected_dir
+mkdir -p $reprojected_dir
 
 for file in `find $mask_rasters_dir -name "*.tif"`
 do
@@ -20,7 +20,8 @@ do
         -r near \
         -ot Byte \
         -overwrite \
-        -t_srs "+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +datum=NAD27 +units=m +no_defs " \
+        -t_srs "+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +datum=NAD27 +units=m +no_defs" \
+        -wo INIT_DEST=0 \
         "$file" \
         "$reprojected_dir/$(basename $file)"
 done
